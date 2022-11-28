b4f350de73c7fba8b2b90a75afcadba7c0760182
eatmorepies-dev <alexandermsummers@gmail.com>
2022-11-28 14:44:57 -0500

    Submitted 10/11/2019. Score earned was 50/50

---
+++ Weather_summersa/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace weatherapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lstbxFirstCityInfo = new System.Windows.Forms.ListBox();
+            this.txtCity1 = new System.Windows.Forms.TextBox();
+            this.txtCity2 = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblFirstCity = new System.Windows.Forms.Label();
+            this.lblSecondCity = new System.Windows.Forms.Label();
+            this.lblWeatherData = new System.Windows.Forms.Label();
+            this.lstbxSecondCityInfo = new System.Windows.Forms.ListBox();
+            this.lstbxComparer = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(184, 488);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(646, 40);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lstbxFirstCityInfo
+            // 
+            this.lstbxFirstCityInfo.FormattingEnabled = true;
+            this.lstbxFirstCityInfo.HorizontalScrollbar = true;
+            this.lstbxFirstCityInfo.ItemHeight = 20;
+            this.lstbxFirstCityInfo.Location = new System.Drawing.Point(50, 149);
+            this.lstbxFirstCityInfo.Name = "lstbxFirstCityInfo";
+            this.lstbxFirstCityInfo.Size = new System.Drawing.Size(424, 224);
+            this.lstbxFirstCityInfo.TabIndex = 1;
+            // 
+            // txtCity1
+            // 
+            this.txtCity1.Location = new System.Drawing.Point(135, 82);
+            this.txtCity1.Name = "txtCity1";
+            this.txtCity1.Size = new System.Drawing.Size(138, 26);
+            this.txtCity1.TabIndex = 2;
+            this.txtCity1.Validating += new System.ComponentModel.CancelEventHandler(this.txtCity1_Validating);
+            // 
+            // txtCity2
+            // 
+            this.txtCity2.Location = new System.Drawing.Point(627, 82);
+            this.txtCity2.Name = "txtCity2";
+            this.txtCity2.Size = new System.Drawing.Size(138, 26);
+            this.txtCity2.TabIndex = 3;
+            this.txtCity2.TextChanged += new System.EventHandler(this.txtCity2_TextChanged);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(184, 560);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(646, 40);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblFirstCity
+            // 
+            this.lblFirstCity.AutoSize = true;
+            this.lblFirstCity.Location = new System.Drawing.Point(45, 86);
+            this.lblFirstCity.Name = "lblFirstCity";
+            this.lblFirstCity.Size = new System.Drawing.Size(70, 20);
+            this.lblFirstCity.TabIndex = 5;
+            this.lblFirstCity.Text = "First City";
+            // 
+            // lblSecondCity
+            // 
+            this.lblSecondCity.AutoSize = true;
+            this.lblSecondCity.Location = new System.Drawing.Point(506, 86);
+            this.lblSecondCity.Name = "lblSecondCity";
+            this.lblSecondCity.Size = new System.Drawing.Size(94, 20);
+            this.lblSecondCity.TabIndex = 6;
+            this.lblSecondCity.Text = "Second City";
+            // 
+            // lblWeatherData
+            // 
+            this.lblWeatherData.AutoSize = true;
+            this.lblWeatherData.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWeatherData.Location = new System.Drawing.Point(32, 12);
+            this.lblWeatherData.Name = "lblWeatherData";
+            this.lblWeatherData.Size = new System.Drawing.Size(204, 20);
+            this.lblWeatherData.TabIndex = 7;
+            this.lblWeatherData.Text = "Weather Data Comparer";
+            // 
+            // lstbxSecondCityInfo
+            // 
+            this.lstbxSecondCityInfo.FormattingEnabled = true;
+            this.lstbxSecondCityInfo.ItemHeight = 20;
+            this.lstbxSecondCityInfo.Location = new System.Drawing.Point(483, 149);
+            this.lstbxSecondCityInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstbxSecondCityInfo.Name = "lstbxSecondCityInfo";
+            this.lstbxSecondCityInfo.Size = new System.Drawing.Size(424, 224);
+            this.lstbxSecondCityInfo.TabIndex = 8;
+            // 
+            // lstbxComparer
+            // 
+            this.lstbxComparer.FormattingEnabled = true;
+            this.lstbxComparer.ItemHeight = 20;
+            this.lstbxComparer.Location = new System.Drawing.Point(50, 385);
+            this.lstbxComparer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstbxComparer.Name = "lstbxComparer";
+            this.lstbxComparer.Size = new System.Drawing.Size(858, 84);
+            this.lstbxComparer.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(152)))), ((int)(((byte)(210)))));
+            this.ClientSize = new System.Drawing.Size(982, 617);
+            this.Controls.Add(this.lstbxComparer);
+            this.Controls.Add(this.lstbxSecondCityInfo);
+            this.Controls.Add(this.lblWeatherData);
+            this.Controls.Add(this.lblSecondCity);
+            this.Controls.Add(this.lblFirstCity);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtCity2);
+            this.Controls.Add(this.txtCity1);
+            this.Controls.Add(this.lstbxFirstCityInfo);
+            this.Controls.Add(this.btnCalculate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Weather Data Comparer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.ListBox lstbxFirstCityInfo;
+        private System.Windows.Forms.TextBox txtCity1;
+        private System.Windows.Forms.TextBox txtCity2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblFirstCity;
+        private System.Windows.Forms.Label lblSecondCity;
+        private System.Windows.Forms.Label lblWeatherData;
+        private System.Windows.Forms.ListBox lstbxSecondCityInfo;
+        private System.Windows.Forms.ListBox lstbxComparer;
+    }
+}
+
---
+++ Weather_summersa/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;  // Added to use the XDocument class
+/*
+ * Alexander Summers
+ * Displays weather info and comparison between two cities.
+ * 10-7-2019
+ */
+namespace weatherapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Enter your API key here.
+        // Get an API key by making a free account at:
+        //      http://home.openweathermap.org/users/sign_in
+        private const string API_KEY = "61ad9d42b5bf4553aba08ad86af729ee";
+
+        // Query URLs. Replace @LOC@ with the location.
+        private const string CurrentUrl =
+            "http://api.openweathermap.org/data/2.5/weather?" +
+            "q=@LOC@&mode=xml&units=imperial&APPID=" + API_KEY;
+
+        
+        static private void GetConditions(string city, ref string temp, ref string units, ref string humidity, ref string pressure)
+        {
+            // Compose the query URL.
+            string url = CurrentUrl.Replace("@LOC@", city);
+            
+
+            // Load the XDocument class with XML file for the requested city
+            XDocument X = XDocument.Load(url);
+
+            // Extract the temperature data from the XML file.
+            XElement tempData = X.Element("current").Element("temperature");
+            XElement humidityData = X.Element("current").Element("humidity");
+            XElement pressureData = X.Element("current").Element("pressure");
+
+            // Extract the first attribute which is the temperature and the last which is the units
+            temp = tempData.FirstAttribute.ToString();
+            units = tempData.LastAttribute.ToString();
+            humidity = humidityData.FirstAttribute.ToString();
+            pressure = pressureData.FirstAttribute.ToString();
+            
+        }
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lstbxFirstCityInfo.Items.Clear();  //While the listbox is clear when the program first starts, it needs to clear itself when "Calculate" is clicked another time. Otherwise, text will simply be added to the end.
+                // Variable declarations
+                string city1, city2;
+                string tempCity1, tempCity2;
+                string unitsCity1, unitsCity2;
+                string humidityCity1, humidityCity2;
+                string pressureCity1, pressureCity2;
+
+                double c1Temp = 0, c2Temp = 0;
+                double c1Humidity = 0, c2Humidity = 0;
+                double c1Pressure = 0, c2Pressure = 0;
+
+                tempCity1 = tempCity2 = unitsCity1 = unitsCity2 = "";
+                humidityCity1 = humidityCity2 = pressureCity1 = pressureCity2 = "";
+
+                // Ask the user for city to get weather information
+
+                city1 = txtCity1.Text;
+
+                // Get the current weather for city1
+                GetConditions(city1, ref tempCity1, ref unitsCity1, ref humidityCity1, ref pressureCity1);
+
+                // Clean up data
+                tempCity1 = tempCity1.Remove(0, 7);
+                tempCity1 = tempCity1.TrimEnd('"');
+                c1Temp = double.Parse(tempCity1);
+
+                
+                unitsCity1 = unitsCity1.Remove(0, 6);
+                unitsCity1 = unitsCity1.TrimEnd('"');
+
+                humidityCity1 = humidityCity1.Remove(0, 7);
+                humidityCity1 = humidityCity1.TrimEnd('"');
+                c1Humidity = double.Parse(humidityCity1);
+
+                pressureCity1 = pressureCity1.Remove(0, 7);
+                pressureCity1 = pressureCity1.TrimEnd('"');
+                c1Pressure = double.Parse(pressureCity1);
+
+                // Adk the user for city to get weather information
+
+                city2 = txtCity2.Text;
+
+                // Get the current weather for city2
+                GetConditions(city2, ref tempCity2, ref unitsCity2, ref humidityCity2, ref pressureCity2);
+
+                // Clean up data
+                tempCity2 = tempCity2.Remove(0, 7);
+                tempCity2 = tempCity2.TrimEnd('"');
+                c2Temp = double.Parse(tempCity2);
+
+                unitsCity2 = unitsCity2.Remove(0, 6);
+                unitsCity2 = unitsCity2.TrimEnd('"');
+
+                humidityCity2 = (humidityCity2.Remove(0, 7)).TrimEnd('"');
+                c2Humidity = double.Parse(humidityCity2);
+
+                c2Pressure = double.Parse((pressureCity2.Remove(0, 7)).TrimEnd('"'));
+
+                // Output data to the screen
+
+                lstbxFirstCityInfo.Items.Add($"{city1}:\n"); //adds info for the first city on the screen
+                lstbxFirstCityInfo.Items.Add($"\t    temp: {c1Temp:N2}\n");  
+                lstbxFirstCityInfo.Items.Add($"\t   units: {unitsCity1}\n");
+                lstbxFirstCityInfo.Items.Add($"\thumidity: {c1Humidity:N0}\n");
+                lstbxFirstCityInfo.Items.Add($"\tpressure: {c1Pressure:N0}");
+                lstbxFirstCityInfo.Items.Add("");
+
+
+                lstbxSecondCityInfo.Items.Add($"{city2}:\n"); //adds info for the second city onto the screen
+                lstbxSecondCityInfo.Items.Add($"\t    temp: {c2Temp:N2}\n");
+                lstbxSecondCityInfo.Items.Add($"\t   units: {unitsCity2}\n");
+                lstbxSecondCityInfo.Items.Add($"\thumidity: {c2Humidity:N0}\n");
+                lstbxSecondCityInfo.Items.Add($"\tpressure: {c2Pressure:N0}");
+                lstbxSecondCityInfo.Items.Add("");
+                // Checking to see if city1 is hotter than city2
+
+
+
+                if (c1Temp > c2Temp)
+                {
+
+                    lstbxComparer.Items.Add($"\t{city1} is hotter than {city2}.");
+                }
+                else
+                {
+
+                    lstbxComparer.Items.Add($"\t{city2} is hotter than {city1}.");
+                }
+
+                // Variant 1
+                /*
+                if (c1Temp > c2Temp)
+                    lstbxWeatherInfo.Items.Add($"\t{city1} is hotter than {city2}.");
+                else
+                    lstbxWeatherInfo.Items.Add($"\t{city2} is hotter than {city1}.");
+
+                // Variant 2
+
+                if (c1Temp > c2Temp) lstbxWeatherInfo.Items.Add($"\t{city1} is hotter than {city2}.");
+                else lstbxWeatherInfo.Items.Add($"\t{city2} is hotter than {city1}.");
+
+                // Check to see if city1 is hotter, equal to, or cooler
+
+                lstbxWeatherInfo.Items.Add("Is city1 hotter, equal to, or cooler than city2");
+
+
+                if (c1Temp > c2Temp)
+                {
+                    lstbxWeatherInfo.Items.Add($"\t{city1} is hotter than {city2}.");
+                }
+                else
+                {
+                    if (c1Temp == c2Temp)
+                    {
+                        lstbxWeatherInfo.Items.Add($"\t{city1} has the same temperature as {city2}.");
+                    }
+                    else
+                    {
+                        lstbxWeatherInfo.Items.Add($"\t{city1} is cooler than {city2}.");
+                    }
+                }
+                */
+                // Variant 3
+                if (c1Temp > c2Temp)
+                {
+                    lstbxComparer.Items.Add($"\t{city1} is hotter than {city2}.");
+                }
+                else if (c1Temp == c2Temp)
+                {
+                    lstbxComparer.Items.Add($"\t{city1} has the same temperature as {city2}.");
+                }
+                else
+                {
+                    lstbxComparer.Items.Add($"\t{city1} is cooler than {city2}.");
+                }
+
+                if (c1Pressure <= 500)
+                {
+                    lstbxFirstCityInfo.Items.Add($"{city1} has a pressure that is less than or equal to 500.");
+                }
+                else if (c1Pressure <= 1000)
+                {
+                    lstbxFirstCityInfo.Items.Add($"{city1} has a pressure that is greater than 500 and less than 1001.");
+                }
+                else
+                {
+                    lstbxFirstCityInfo.Items.Add($"{city1} has a pressure that is greater than 1000.");
+                }
+
+                if (c2Pressure <= 500)
+                {
+                    lstbxSecondCityInfo.Items.Add($"{city2} has a pressure that is less than or equal to 500.");
+                }
+                else if (c1Pressure <= 1000)
+                {
+                    lstbxSecondCityInfo.Items.Add($"{city2} has a pressure that is greater than 500 and less than 1001.");
+                }
+                else
+                {
+                    lstbxSecondCityInfo.Items.Add($"{city2} has a pressure that is greater than 1000.");
+                }
+                /*
+                 * Determine humidity range for City 1
+                 *   Less than or equal to 50
+                 *   between 51 and 100
+                 *   greater than or equal to 101
+                 */
+
+
+                lstbxFirstCityInfo.Items.Add("*** City1 humidity range ***");
+
+
+                if (c1Humidity <= 50)
+                    lstbxFirstCityInfo.Items.Add($"{city1}'s humidity is less than or equal to 50.");
+                else if (c1Humidity > 50 && c1Humidity <= 100)
+                    lstbxFirstCityInfo.Items.Add($"{city1}'s humidity is between 50 and 100.");
+                else
+                    lstbxFirstCityInfo.Items.Add($"{city1}'s humidity is greater than 100.");
+
+                lstbxSecondCityInfo.Items.Add("*** City2 humidity range ***");
+                if (c2Humidity <= 50)
+                    lstbxSecondCityInfo.Items.Add($"{city2}'s humidity is less than or equal to 50.");
+                else if (c2Humidity > 50 && c2Humidity <= 100)
+                    lstbxSecondCityInfo.Items.Add($"{city2}'s humidity is between 50 and 100.");
+                else
+                    lstbxSecondCityInfo.Items.Add($"{city2}'s humidity is greater than 100.");
+
+            }
+            catch    
+            {
+                MessageBox.Show("Please make sure your city names are valid.");
+                lstbxFirstCityInfo.Items.Clear();
+            }
+           
+}
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ListBox temp;
+
+            foreach (Control c in this.Controls)
+            {
+                if (c is ListBox)
+                {
+                    temp = (ListBox)c;
+                    temp.Items.Clear();
+                }
+            }
+          
+        }
+
+        private void txtCity1_Validating(object sender, CancelEventArgs e)
+        {
+            if (int.TryParse(txtCity1.Text, out int num))
+            {
+                MessageBox.Show("Please enter in a city name in the upper leftmost box.");
+                txtCity1.Clear();
+                txtCity1.Focus();
+                return;
+            }
+        }
+
+        private void txtCity2_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtCity1.Text, out int num))
+            {
+                MessageBox.Show("Please enter in a city name in the upper rightmost box.");
+                txtCity2.Clear();
+                txtCity2.Focus();
+                return;
+            }
+        }
+    }
+}
+        
+    
+
